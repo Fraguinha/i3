238dc68ba481899eedd3d5e9157771388b38f83d
Joao Fraga <joao.fraga@feedzai.com>
2024-01-11 12:05:33 +0000

    Update configurations

---
+++ scripts/toggle_theme.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+if [ "$(gsettings get org.gnome.desktop.interface color-scheme)" = "'prefer-light'" ]; then
+	gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+else
+	gsettings set org.gnome.desktop.interface color-scheme prefer-light
+fi
